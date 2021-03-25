2d18ee2f72e593a822c78f00c5646826d9b0def5
Riceroll <jianzhe.gu@gmail.com>
2021-03-25 01:53:53 -0400

    fix uploading

--- pneumesh2/main.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Viewer from './viewer';
-
-test('renders learn react link', () => {
-  render(<Viewer />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
